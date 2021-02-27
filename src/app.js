a73705c71c2afbb141edd6b0bba019a86085756a
LomakinaNataliia <lomakinataliia@gmail.com>
2021-02-27 22:33:31 +0200

    added content, changed js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ function checkAnswers() {
   var rightAnswer = document.getElementsByClassName("right");
   var wrongAnswer = document.getElementsByClassName("wrong");
 
-  for (let index = 0; index < rightAnswer.length; index++) {
+  for (let index = 0; index < checkButton.length; index++) {
     rightAnswer[index].className += " checked-right";
-  }
-
-  for (let index = 0; index < wrongAnswer.length; index++) {
     wrongAnswer[index].className += " checked-wrong";
+    //     rightAnswer.classList.add("checked-right");
+    //     wrongAnswer.classList.add("checked-wrong");
   }
 }
-let checkButton = document.querySelector("#check");
-checkButton.addEventListener("click", checkAnswers);
+
+// let checkZone = document.getElementsByTagName("form");
+let checkButton = document.getElementsByTagName("input");
+for (let i = 0; i < checkButton.length; i++) {
+  checkButton[i].addEventListener("click", checkAnswers);
+}
