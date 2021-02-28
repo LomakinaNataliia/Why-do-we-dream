3ed2e14517c845525fd8e19c8dc0a5b47a0a6b40
LomakinaNataliia <lomakinataliia@gmail.com>
2021-02-28 22:09:44 +0200

    added js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,58 @@
-function checkAnswers() {
-  var rightAnswer = document.getElementsByClassName("right");
-  var wrongAnswer = document.getElementsByClassName("wrong");
+function click() {
+  clearAll();
+  showQuestion();
+}
 
-  for (let index = 0; index < checkButton.length; index++) {
-    rightAnswer[index].className += " checked-right";
-    wrongAnswer[index].className += " checked-wrong";
-    //     rightAnswer.classList.add("checked-right");
-    //     wrongAnswer.classList.add("checked-wrong");
+function clearAll() {
+  for (let index = 0; index < rightAnswer.length; index++) {
+    rightAnswer[index].classList.remove("checked-right");
+    // rightAnswer[index].className -= " checked-right";
+  }
+
+  for (let index = 0; index < wrongAnswer.length; index++) {
+    // wrongAnswer[index].className -= " checked-wrong";
+    wrongAnswer[index].classList.remove("checked-wrong");
   }
 }
 
-// let checkZone = document.getElementsByTagName("form");
+function showQuestion() {
+  if (document.getElementById("question1").style.display == "block") {
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "block";
+  } else if (document.getElementById("question2").style.display == "block") {
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "block";
+  } else if (document.getElementById("question3").style.display == "block") {
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "block";
+  } else {
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "block";
+  }
+}
+
+function checkAnswers() {
+  // let checkZone = document.getElementsByTagName("input").parent;
+  // // console.log(checkZone);
+
+  for (let index = 0; index < rightAnswer.length; index++) {
+    rightAnswer[index].classList.add("checked-right");
+    // rightAnswer[index].className += " checked-right";
+  }
+
+  for (let index = 0; index < wrongAnswer.length; index++) {
+    // wrongAnswer[index].className += " checked-wrong";
+    wrongAnswer[index].classList.add("checked-wrong");
+  }
+}
+
+var rightAnswer = document.getElementsByClassName("right");
+var wrongAnswer = document.getElementsByClassName("wrong");
 let checkButton = document.getElementsByTagName("input");
+// let checkZone = document.getElementsByTagName("form");
 for (let i = 0; i < checkButton.length; i++) {
   checkButton[i].addEventListener("click", checkAnswers);
 }
+
+let nextQuestion = document.querySelector("#next");
+nextQuestion.addEventListener("click", click);
